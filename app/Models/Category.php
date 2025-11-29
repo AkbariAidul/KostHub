21a98537e8ceb0270f3ce374e556f481ac02152b
AkbariAidul <akbariaidul@gmail.com>
2025-11-29 20:47:58 +0800

    feat(home): update home page to display dynamic kos counts by category and city

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
@@ -16,4 +17,10 @@ class Category extends Model
         'name',
         'slug',
     ];
+
+    public function boardingHouses(): HasMany
+    {
+        // Artinya: Satu Category memiliki banyak BoardingHouse
+        return $this->hasMany(BoardingHouse::class);
+    }
 }
