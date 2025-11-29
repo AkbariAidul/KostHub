83824c1c2e8cbe6d5577eb41108c7004b941f1a4
AkbariAidul <akbariaidul@gmail.com>
2025-11-29 11:01:24 +0800

    feat(ui): add main layout, pages structure, and integrate frontend assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-        <script src="{{ asset ('assets/js/index.js') }}"></script>
+        <script src="{{ asset ('assets/jsindex.js') }}"></script>
 
     </body>
 
